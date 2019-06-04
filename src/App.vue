8c4ce107f6094cd8c03b256cd34eec0142b3d71b
jessicalynhansen@gmail.com <jessicalynhansen@gmail.com>
2019-06-04 15:28:32 -0600

    Style header

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header msg="SYNONYMS"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
@@ -21,8 +21,8 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 0;
+  box-sizing: border-box;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="header">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  font-size: 40px;
+  padding-left: 20px;
+}
+</style>
